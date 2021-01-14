cb8161c11e6e0e4bd8f57d38851cedf764496558
Sal Tamay <sal.tamay@outlook.com>
2021-01-13 22:19:12 -0500

    Get human data from form input

--- app.js
+++ app.js
@@ -1,23 +1,84 @@
 // Create Dino Constructor
 'use strict'
 
-const App = (function (model = null, view = null, controller = null) {
-  // console.log(data)
-  // console.log(Model.getDinos())
+const DinoCtrl = (function () {
+  // Dinos Data
+  // Create Dino Object Instances
+  const dinos = data.map(function (dino) {
+    const { species, weight, height, diet, where, when, fact } = dino
+    return new Dino(species, weight, height, diet, where, when, fact)
+  })
 
   return {
-    init: function () {
-      // Data
-      // Create Dino Object Instances
-      const dinos = data.map(function (dino) {
-        const { species, weight, height, diet, where, when, fact } = dino
-        return new Dino(species, weight, height, diet, where, when, fact)
-      })
-      console.log('Starting...')
-      console.log(dinos)
+    getAllDinos: function () {
+      return dinos
     },
   }
 })()
+
+const HumanCtrl = (function () {
+  return {
+    createHuman: function (name, weight, height, diet) {
+      return new Human(name, weight, height, diet)
+    },
+  }
+})()
+
+const UICtrl = (function () {
+  const UISelectors = {
+    name: '#name',
+    feet: '#feet',
+    inches: '#inches',
+    weight: '#weight',
+    diet: '#diet',
+  }
+
+  return {
+    getFormData: function () {
+      return {
+        name: document.querySelector(UISelectors.name).value,
+        feet: parseInt(document.querySelector(UISelectors.feet).value),
+        inches: parseInt(document.querySelector(UISelectors.inches).value),
+        weight: parseInt(document.querySelector(UISelectors.weight).value),
+        diet: document.querySelector(UISelectors.diet).value,
+      }
+    },
+  }
+})()
+
+const App = (function (dinoController, humanController, uiController) {
+  // Load event listeners
+  let appData = {
+    dinos: null,
+    user: null,
+  }
+
+  const loadEventListeners = function () {
+    document.querySelector('#btn').addEventListener('click', getUserInput)
+  }
+
+  const getUserInput = function (e) {
+    const formData = UICtrl.getFormData()
+    const [name, feet, inches, weight, diet] = Object.values(formData)
+    const height = feetToInchConvertor(feet) + inches
+    appData.user = HumanCtrl.createHuman(name, weight, height, diet)
+    console.log(typeof appData.user)
+    e.preventDefault()
+  }
+
+  const feetToInchConvertor = function (feet) {
+    return feet * 12
+  }
+
+  return {
+    init: function () {
+      console.log('Starting...')
+      appData.dinos = DinoCtrl.getAllDinos()
+      // Load event listeners
+      loadEventListeners()
+    },
+  }
+})(DinoCtrl, HumanCtrl, UICtrl)
 
 App.init()
 
